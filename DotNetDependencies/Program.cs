f3e0dbfb82706758a28af7d2f36d0403c84f7c37
Jonathan Cumini <jonathan.cumini@bairesdev.com>
2023-01-24 19:24:49 -0300

    Updated folder structure. Small change

---
+++ DotNetDependencies/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+
+int result = Fibonacci(4);
+Console.WriteLine("The 4th Fibonacci number is " + result);
+
+static int Fibonacci(int n)
+{
+    Debug.WriteLine($"Entering {nameof(Fibonacci)} method");
+    Debug.WriteLine($"We are looking for the {n}th number");
+
+    int n1 = 0;
+    int n2 = 1;
+    int sum;
+
+    for (int i = 1; i <= n; i++)
+    {
+        sum = n1 + n2;
+        n1 = n2;
+        n2 = sum;
+        Debug.WriteLine(sum ==1, $"sum is 1, n1 is {n1}, n2 is {n2}");    
+    }
+
+    // If n2 is 5 continue, else break.
+    Debug.Assert(n2 == 5, "The return value is not 5 and it should be.");
+    return n == 0 ? n1 : n2;
+}
