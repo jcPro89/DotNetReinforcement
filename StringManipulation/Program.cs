c8e6c201e87da503e36fb50b1346fda5d8a65237
Jonathan Cumini <jonathancumini@gmail.com>
2023-01-27 07:17:07 -0300

    Added clarifications

--- StringManipulation/Program.cs
+++ StringManipulation/Program.cs
@@ -4,24 +4,25 @@
     {
         int myYearOfBirth = 1989;
         int myAge = DateTime.Now.Year - myYearOfBirth;
-        var myHeight = 1.86m; //literal decimal 
         int myWeight = 67;
+        var myHeight = 1.86d; //literal double. Use m for decimal 
+        
+        string dataDirectory = "C:\\instrument"; // backslash character escape \\
 
         Console.Write($"I'm {myAge} years old. "); //String interpolation        
         Console.WriteLine($"Next year, I'll be {++myAge}."); //addition assignment with increment before retrieving the variable's value
 
         /* Quotient (left of the assignment operator) AND 
          * either the dividend or divisor must be of type decimal (or both) */
-        decimal myImc = myWeight / (myHeight*myHeight); 
-        Console.WriteLine($"I'm {myHeight}m tall. I have an IMC of {myImc}.");
+        decimal myImc = myWeight / (decimal)(Math.Pow(myHeight, 2)); 
+        Console.WriteLine($"I'm {myHeight}m tall. I have an IMC of " + string.Format("{0:0.0}", myImc));
 
         Console.WriteLine("My diminutives are \"Jona\", \"Jony\" and \"Jonita\"\n"); // double-quotation mark character escape \"
-
-        string dataDirectory = "C:\\instrument"; // backslash character escape \\
-        Console.WriteLine($"The paths for my application settings and output is {dataDirectory}, and these are the subdirectories:\n");        
+        
+        Console.WriteLine("The paths for my application settings and output is " + dataDirectory); // String concatenation        
         dataDirectory = @"C:\instrument\data  C:\instrument\config
-            C:\instrument\log"; // Verbatim string literal
-        Console.WriteLine($@"And again...The path for my application settings and output is {dataDirectory}");
+            C:\instrument\log"; // Verbatim string literal. Does not need character escapes. Takes also tabs and carriage returns characters
+        Console.WriteLine($@"And again...The paths for my application settings and output are {dataDirectory}"); // String interpolation and verbatima string literal
 
         PrintMyCategories();
         _ = Console.Read();
