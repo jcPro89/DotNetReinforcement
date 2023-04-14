9786380f6b649a7be8fd8084e6675ee67949d288
Jonathan Cumini <jonathan.cumini@gmail.com>
2023-04-14 09:11:43 -0300

    Adding ASP.NET Core projects used for my day job

---
+++ WebApplication1/ActivitiesDataContext.cs
@@ -0,0 +1,13 @@
+﻿using ActivityAwardsApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ActivityAwardsApp
+{
+    public class ActivitiesDataContext : DbContext
+    {
+        public ActivitiesDataContext(DbContextOptions<ActivitiesDataContext> options) : base(options) { }
+
+        public DbSet<Activity> Activities { get; set; }
+        public DbSet<Award> Awards { get; set; }
+    }
+}
